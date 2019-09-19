29e3f47fc9334073f0539d2fbbd41cbedcaf8759
Eduardo <edu.m.oliveira1@gmail.com>
2019-09-19 16:28:02 -0300

    Alteração

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,14 +1,14 @@
-importScripts('https://www.gstatic.com/firebasejs/6.6.1/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/5.9.1/firebase-messaging.js');
+importScripts('https://www.gstatic.com/firebasejs/3.9.0/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/3.9.0/firebase-messaging.js');
 
 firebase.initializeApp({
-    'messagingSenderId': "824658242094"  
+    'messagingSenderId': "582721320268"
 });
 
 const messaging = firebase.messaging();
 
-messaging.setBackgroundMessageHandler(function(payload){
-    console.log('Received background message', payload);
-    return self.registration.showNotification({},{});
-})
+messaging.setBackgroundMessageHandler(function (payload) {
+    console.log('Received background message ', payload);
+    return self.registration.showNotification({}, {});
+});
 
