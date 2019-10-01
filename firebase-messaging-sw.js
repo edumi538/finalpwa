482453c41c0d465750b60b5d1238205cc4801f6a
Eduardo <edu.m.oliveira1@gmail.com>
2019-10-01 14:47:59 -0300

    modification

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -2,7 +2,7 @@ importScripts('https://www.gstatic.com/firebasejs/3.9.0/firebase-app.js');
 importScripts('https://www.gstatic.com/firebasejs/3.9.0/firebase-messaging.js');
 
 firebase.initializeApp({
-    'messagingSenderId': "582721320268"
+    'messagingSenderId': "824658242094"
 });
 
 const messaging = firebase.messaging();
