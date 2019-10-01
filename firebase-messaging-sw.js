f28bf29883fe16289b617f8eef18bfa0a517c74b
Eduardo <edu.m.oliveira1@gmail.com>
2019-10-01 15:17:02 -0300

    .

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -7,9 +7,9 @@ importScripts('https://www.gstatic.com/firebasejs/4.8.1/firebase-messaging.js');
 // Initialize the Firebase app in the service worker by passing in the
 // messagingSenderId.
 firebase.initializeApp({
-  'messagingSenderId': 'YOUR-SENDER-ID'
+  'messagingSenderId': '824658242094'
 });
 
 // Retrieve an instance of Firebase Messaging so that it can handle background
 // messages.
-const messaging = firebase.messaging();firebase-messaging-sw.js
+const messaging = firebase.messaging();
